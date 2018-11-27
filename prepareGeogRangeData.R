6a0f69bd5a8a493b5470d4a867f3aa32a770af3f
Jun Ying Lim <junyinglim@gmail.com>
2018-11-27 14:06:22 +0100

    Added some correlated MCMC moves to help diversification rate and origin time and branch rate mixing

--- prepareGeogRangeData.R
+++ prepareGeogRangeData.R
@@ -1,10 +1,10 @@
 library(ape)
 
 main.dir <- "/Users/junyinglim/Dropbox/spiders/phylo/ultros/"
-output.dir <- "/Users/junyinglim/Dropbox/spiders/phylo/jointBiogeogDating/data"
+output.dir <- "/Users/junyinglim/Dropbox/spiders/phylo/tetragnathaBiogeogDating/data"
 
 biogeog<- read.csv(file.path(main.dir, "ultros_islands_SK.csv"))
 biogeog_subset <- biogeog[c("Co","Ka","Oa","Ma", "Ha")]
-rownames(biogeog_subset) <- biogeog_subset$specimen_UID
+rownames(biogeog_subset) <- biogeog$specimen_UID
 
 write.nexus.data(as.matrix(biogeog_subset), file.path(output.dir, "spiders_biogeog.nex"), format = "Standard")
